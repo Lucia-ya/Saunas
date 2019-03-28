bd9390b58eedd5a64407f01d50bc7f1eb414449c
Lucia <lisen0k861@gmail.com>
2019-03-29 01:15:01 +0500

    v2

--- app/src/main/java/com/luciaya/saunas/SplashActivity.java
+++ app/src/main/java/com/luciaya/saunas/SplashActivity.java
@@ -1,21 +1,37 @@
 package com.luciaya.saunas;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
 
 /**
  *  Splash - ScreenВыводится темой - бэкграунд картинка пока приложение грузится
  */
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
+    private final int SPLASH_DISPLAY_LENGHT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
